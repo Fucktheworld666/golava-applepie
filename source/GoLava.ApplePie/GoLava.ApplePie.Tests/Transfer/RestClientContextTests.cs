ff6d2fa24ab134ed95ffbe529bdd558dfb012823
Sascha Kiefer <esskar@golava.io>
2017-12-26 18:12:26 -0200

    add RestClientContext documentation and tests

---
+++ source/GoLava.ApplePie/GoLava.ApplePie.Tests/Transfer/RestClientContextTests.cs
@@ -0,0 +1,15 @@
+﻿using GoLava.ApplePie.Transfer;
+using Xunit;
+
+namespace GoLava.ApplePie.Tests.Transfer
+{
+    public class RestClientContextTests
+    {
+        [Fact]
+        public void CookieJarIsNotNull()
+        {
+            var context = new RestClientContext();
+            Assert.NotNull(context.CookieJar);
+        }
+    }
+}
--- source/GoLava.ApplePie/GoLava.ApplePie/Transfer/RestClientContext.cs
+++ source/GoLava.ApplePie/GoLava.ApplePie/Transfer/RestClientContext.cs
@@ -2,13 +2,23 @@
 
 namespace GoLava.ApplePie.Transfer
 {
+    /// <summary>
+    /// A context to be shared between multiple rest calls to keep track of
+    /// things like session, cookies and the like.
+    /// </summary>
     public class RestClientContext
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:RestClientContext"/> class.
+        /// </summary>
         public RestClientContext()
         {
             this.CookieJar = new CookieJar();
         }
 
-        public CookieJar CookieJar { get; set; }
+        /// <summary>
+        /// Gets the <see cref="T:CookieJar"/> associated with this <see cref="T:RestClientContext"/>.
+        /// </summary>
+        public CookieJar CookieJar { get; }
     }
 }
