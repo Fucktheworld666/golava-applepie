2ed4e5c826930464ee2533e8eb8af01e2c40a348
Sascha Kiefer <esskar@golava.io>
2017-12-26 14:06:25 -0200

    HttpMessageHandlerExtensions documentation and unit tests

---
+++ source/GoLava.ApplePie/GoLava.ApplePie.Tests/Handlers/HttpMessageHandlerExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using GoLava.ApplePie.Transfer.Handlers;
+using Xunit;
+
+namespace GoLava.ApplePie.Tests.Handlers
+{
+    public class HttpMessageHandlerExtensionsTests
+    {
+        [Fact]
+        public void PassingNullAsOuterHandlerWillRaiseArgumentNullException()
+        {
+            DelegatingHandler outerHandler = null;
+            var exception = Assert.Throws<ArgumentNullException>(() => new HttpClientHandler().DecorateWith(outerHandler));
+            Assert.Equal(nameof(outerHandler), exception.ParamName);
+        }
+
+        [Fact]
+        public void InnerHandlerAreSameInstance()
+        {
+            var innerHandler = new HttpClientHandler();
+            var outerHandler = new TestDelegatingHandler();
+
+            innerHandler.DecorateWith(outerHandler);
+
+            Assert.Same(innerHandler, outerHandler.InnerHandler);
+        }
+
+        [Fact]
+        public void OuterHandlerAndResultAreSameInstance()
+        {
+            var innerHandler = new HttpClientHandler();
+            var outerHandler = new TestDelegatingHandler();
+
+            var result = innerHandler.DecorateWith(outerHandler);
+
+            Assert.Same(outerHandler, result);
+        }
+
+        public class TestDelegatingHandler : DelegatingHandler { }
+    }
+}
--- source/GoLava.ApplePie/GoLava.ApplePie/Transfer/Handlers/HttpMessageHandlerExtensions.cs
+++ source/GoLava.ApplePie/GoLava.ApplePie/Transfer/Handlers/HttpMessageHandlerExtensions.cs
@@ -1,12 +1,24 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 
 namespace GoLava.ApplePie.Transfer.Handlers
 {
+    /// <summary>
+    /// Http message handler extensions.
+    /// </summary>
     public static class HttpMessageHandlerExtensions
     {
+        /// <summary>
+        /// Assigns the inner handler to the <see cref="P:DelegatingHandler.InnerHandler"/> of the outer handler. 
+        /// </summary>
+        /// <returns>The the outer handler.</returns>
+        /// <param name="innerHandler">The inner  handler to be assigned to the <see cref="P:DelegatingHandler.InnerHandler"/> of the outer handler.</param>
+        /// <param name="outerHandler">The outer handler that will be become the parent of the inner handler.</param>
         public static T DecorateWith<T>(this HttpMessageHandler innerHandler, T outerHandler)
             where T: DelegatingHandler 
         {
+            if (outerHandler == null)
+                throw new ArgumentNullException(nameof(outerHandler));
             outerHandler.InnerHandler = innerHandler;
             return outerHandler;
         }
