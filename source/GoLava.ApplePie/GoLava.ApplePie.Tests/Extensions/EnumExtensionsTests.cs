dd751c1e7d3abcf794bb3f6372290902fd707c6d
Sascha Kiefer <esskar@golava.io>
2017-12-27 00:00:35 -0200

    add EnumExtensions tests

---
+++ source/GoLava.ApplePie/GoLava.ApplePie.Tests/Extensions/EnumExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using GoLava.ApplePie.Extensions;
+using Xunit;
+
+namespace GoLava.ApplePie.Tests.Extensions
+{
+    public class EnumExtensionsTests
+    {
+        [Fact]
+        public void ToDescriptionStringReturnsToStringWithoutDescriptionAttribute()
+        {
+            var a = TestEnum.A;
+            Assert.Equal(a.ToString(), a.ToDescriptionString());
+        }
+
+        [Fact]
+        public void ToDescriptionStringReturnsDescriptionAttribute()
+        {
+            var b = TestEnum.B;
+            Assert.Equal("x", b.ToDescriptionString());
+        }
+
+        private enum TestEnum
+        {
+            A,
+            [Description("x")]B
+        }
+    }
+}
