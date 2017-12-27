062e5c96f8fbdc892c894e680f9a9fbe48908c84
Sascha Kiefer <esskar@golava.io>
2017-12-27 11:52:22 -0200

    add documentation

--- source/GoLava.ApplePie/GoLava.ApplePie/Exceptions/ApplePieException.cs
+++ source/GoLava.ApplePie/GoLava.ApplePie/Exceptions/ApplePieException.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 namespace GoLava.ApplePie.Exceptions
 {
+    /// <summary>
+    /// A ApplePie exception.
+    /// </summary>
     public class ApplePieException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:ApplePieException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the exception.</param>
         public ApplePieException(string message)
             : base(message) { }
 
-        public ApplePieException(string message, Exception exception)
-            : base(message, exception) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:ApplePieException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the exception.</param>
+        /// <param name="innerException">An inner exception that was raised 
+        /// previously to this <see cref="T:ApplePieException"/> instance.</param>
+        public ApplePieException(string message, Exception innerException)
+            : base(message, innerException) { }
     }
 }
--- source/GoLava.ApplePie/GoLava.ApplePie/Threading/Configure.cs
+++ source/GoLava.ApplePie/GoLava.ApplePie/Threading/Configure.cs
@@ -4,15 +4,32 @@ using System.Threading;
 
 namespace GoLava.ApplePie.Threading
 {
+    /// <summary>
+    /// A helper to handle task synchronization more elegant.
+    /// </summary>
     public struct Configure : INotifyCompletion
     {
+        /// <summary>
+        /// Configures an awaiter used to await for upcomping tasks and prevents
+        /// the attempt to marshal the continuations back to the original context 
+        /// captured.
+        /// </summary>
+        /// <returns>The false.</returns>
         public static Configure AwaitFalse()
         {
             return new Configure();
         }
 
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="T:GConfigure"/> instance is completed.
+        /// </summary>
+        /// <value><c>true</c> if is completed; otherwise, <c>false</c>.</value>
         public bool IsCompleted => SynchronizationContext.Current == null;
 
+        /// <summary>
+        /// Schedules the continuation action that's invoked when the instance completes.
+        /// </summary>
+        /// <param name="continuation">The action to invoke when the operation completes.</param>
         public void OnCompleted(Action continuation)
         {
             var prev = SynchronizationContext.Current;
@@ -30,11 +47,18 @@ namespace GoLava.ApplePie.Threading
             }
         }
 
+        /// <summary>
+        /// Gets an awaiter used to await this upcoming tasks.
+        /// </summary>
+        /// <returns>The awaiter.</returns>
         public Configure GetAwaiter()
         {
             return this;
         }
 
+        /// <summary>
+        /// Gets the result.
+        /// </summary>
         public void GetResult()
         {
             // empty on purpose
