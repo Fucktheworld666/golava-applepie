3df226348b59010087cd18e720f46c3019ea89fc
Sascha Kiefer <esskar@golava.io>
2017-12-30 15:58:39 -0200

    make changes needed to serialize all important values within a ClientContext

--- source/GoLava.ApplePie/GoLava.ApplePie/Clients/ClientContext.cs
+++ source/GoLava.ApplePie/GoLava.ApplePie/Clients/ClientContext.cs
@@ -1,28 +1,78 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Text;
 using GoLava.ApplePie.Contracts;
+using GoLava.ApplePie.Contracts.Attributes;
 using GoLava.ApplePie.Transfer;
 
 namespace GoLava.ApplePie.Clients
 {
+    /// <summary>
+    /// Context that keeps data between calls.
+    /// </summary>
     public class ClientContext : RestClientContext
     {
         private readonly ConcurrentDictionary<string, object> _stash;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:ClientContext"/> class.
+        /// </summary>
         public ClientContext()
         {
             _stash = new ConcurrentDictionary<string, object>();
         }
 
+        /// <summary>
+        /// Gets or sets the value of the current authentication state.
+        /// </summary>
         public Authentication Authentication { get; set; }
 
+        /// <summary>
+        /// Gets or sets the auth token to be used during authenication calls.
+        /// </summary>
         public AuthToken AuthToken { get; set; }
 
+        /// <summary>
+        /// Gets or sets the logon authentication information.
+        /// </summary>
         public LogonAuth LogonAuth { get; set; }
 
+        /// <summary>
+        /// Gets or sets information about the current two step authenication process.
+        /// </summary>
         public TwoStepToken TwoStepToken { get; set; }
 
+        /// <summary>
+        /// Gets or sets the session values.
+        /// </summary>
         public Session Session { get; set; }
+
+        /// <summary>
+        /// Gets or sets the csrf tokens.
+        /// </summary>
+        public IEnumerable<CsrfToken> CsrfTokens 
+        {
+            get 
+            {
+                foreach (CsrfClass csrfClass in Enum.GetValues(typeof(CsrfClass)))
+                {
+                    if (this.TryGetValue(out CsrfToken csrfToken, csrfClass))
+                        yield return csrfToken;
+                }
+            }
+            set 
+            {
+                foreach (CsrfClass csrfClass in Enum.GetValues(typeof(CsrfClass)))
+                    this.DeleteValue<CsrfToken>(csrfClass);
+
+                if (value != null)
+                {
+                    foreach (var csrfToken in value)
+                        this.AddValue(csrfToken, csrfToken.Class);
+                }
+            }
+        }
 
         internal void AddValue<T>(T item, params object[] keys)
         {
