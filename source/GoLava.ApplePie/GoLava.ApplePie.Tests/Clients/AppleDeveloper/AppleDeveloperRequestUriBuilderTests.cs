7162db2215c03b89bc6dea8343df5cd48e7bada0
Sascha Kiefer <esskar@golava.io>
2018-01-02 18:01:06 -0200

    Add AppleDeveloperRequestUriBuilder tests

---
+++ source/GoLava.ApplePie/GoLava.ApplePie.Tests/Clients/AppleDeveloper/AppleDeveloperRequestUriBuilderTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using GoLava.ApplePie.Clients.AppleDeveloper;
+using Xunit;
+
+namespace GoLava.ApplePie.Tests.Clients.AppleDeveloper
+{
+    public class AppleDeveloperRequestUriBuilderTests
+    {
+        [Fact]
+        public void BuildUriHasCorrectContentTypeParameter()
+        {
+            var builder = new AppleDeveloperRequestUriBuilder(new Uri("http://www.foo.ext"));
+            var uri = builder.ToUri();
+
+            var query = HttpUtility.ParseQueryString(uri.Query);
+            Assert.Equal("text/x-url-arguments", query["content-type"]);
+        }
+
+        [Fact]
+        public void BuildUriHasCorrectAcceptParameter()
+        {
+            var builder = new AppleDeveloperRequestUriBuilder(new Uri("http://www.foo.ext"));
+            var uri = builder.ToUri();
+
+            var query = HttpUtility.ParseQueryString(uri.Query);
+            Assert.Equal("application/json", query["accept"]);
+        }
+
+        [Fact]
+        public void BuildUriHasCorrectUserLocaleParameter()
+        {
+            var builder = new AppleDeveloperRequestUriBuilder(new Uri("http://www.foo.ext"));
+            var uri = builder.ToUri();
+
+            var query = HttpUtility.ParseQueryString(uri.Query);
+            Assert.Equal("en_US", query["userLocale"]);
+        }
+
+        [Fact]
+        public void BuildUriHasRequestIdParameter()
+        {
+            var builder = new AppleDeveloperRequestUriBuilder(new Uri("http://www.foo.ext"));
+            var uri = builder.ToUri();
+
+            var query = HttpUtility.ParseQueryString(uri.Query);
+            Assert.NotNull(query["requestId"]);
+            Assert.NotEqual(string.Empty, query["requestId"]);
+        }
+
+        [Fact]
+        public void RequestIdChangesWithEveryCallOfToUri()
+        {
+            var builder = new AppleDeveloperRequestUriBuilder(new Uri("http://www.foo.ext"));
+            var uri1 = builder.ToUri();
+            var uri2 = builder.ToUri();
+
+            var query1 = HttpUtility.ParseQueryString(uri1.Query);
+            var query2 = HttpUtility.ParseQueryString(uri2.Query);
+            Assert.NotEqual(query1["requestId"], query2["requestId"]);
+        }
+
+        [Fact]
+        public void AddQueryValuesAddsValuesToTheQuery()
+        {
+            var builder = new AppleDeveloperRequestUriBuilder(new Uri("http://www.foo.ext"));
+            builder.AddQueryValues(new Dictionary<string, string> {
+                { "x", "y" },
+                { "foo", "bar" }
+            });
+            var uri = builder.ToUri();
+
+            var query = HttpUtility.ParseQueryString(uri.Query);
+            Assert.Equal("y", query["x"]);
+            Assert.Equal("bar", query["foo"]);
+        }
+    }
+}
--- source/GoLava.ApplePie/GoLava.ApplePie/Clients/AppleDeveloper/AppleDeveloperRequestUriBuilder.cs
+++ source/GoLava.ApplePie/GoLava.ApplePie/Clients/AppleDeveloper/AppleDeveloperRequestUriBuilder.cs
@@ -6,11 +6,19 @@ using GoLava.ApplePie.Transfer;
 
 namespace GoLava.ApplePie.Clients.AppleDeveloper
 {
+    /// <summary>
+    /// Build URLs for apple developer requests with different query parameters
+    /// </summary>
     public class AppleDeveloperRequestUriBuilder
     {
         private readonly UriBuilder _uriBuilder;
         private readonly NameValueCollection _query;
 
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="T:AppleDeveloperRequestUriBuilder"/> class.
+        /// </summary>
+        /// <param name="uri">URI.</param>
         public AppleDeveloperRequestUriBuilder(Uri uri)
         {
             _uriBuilder = new UriBuilder(uri);
@@ -21,6 +29,9 @@ namespace GoLava.ApplePie.Clients.AppleDeveloper
             _query["userLocale"] = "en_US";
         }
 
+        /// <summary>
+        /// Adds the query values to the URI.
+        /// </summary>
         public AppleDeveloperRequestUriBuilder AddQueryValues(Dictionary<string, string> queryValues)
         {
             if (queryValues != null)
@@ -31,6 +42,9 @@ namespace GoLava.ApplePie.Clients.AppleDeveloper
             return this;
         }
 
+        /// <summary>
+        /// Returns a <see cref="T:String"/> that represents the current <see cref="T:AppleDeveloperRequestUriBuilder"/> state of the URI to build.
+        /// </summary>
         public override string ToString()
         {
             _query["requestId"] = Guid.NewGuid().ToString("D");
@@ -39,6 +53,10 @@ namespace GoLava.ApplePie.Clients.AppleDeveloper
             return _uriBuilder.ToString();
         }
 
+        /// <summary>
+        /// Returns a <see cref="T:RestUri"/>  that represents the current <see cref="T:AppleDeveloperRequestUriBuilder"/> state of the URI to build.
+        /// </summary>
+        /// <returns>The URI.</returns>
         public RestUri ToUri()
         {
             return new RestUri(this.ToString());
