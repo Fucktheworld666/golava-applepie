31b446048969ca6412812858a60569cedb108255
Sascha Kiefer <esskar@golava.io>
2018-04-03 00:57:51 +0200

    add comments and check factory parameters

--- source/GoLava.ApplePie/GoLava.ApplePie/ApplePieOptions.cs
+++ source/GoLava.ApplePie/GoLava.ApplePie/ApplePieOptions.cs
@@ -5,6 +5,9 @@ using GoLava.ApplePie.Security.CertificateStores;
 
 namespace GoLava.ApplePie
 {
+    /// <summary>
+    /// Options to be used for ApplePie.
+    /// </summary>
     public class ApplePieOptions
     {
         internal Func<IServiceProvider, IAppleDeveloperUrlProvider> AppleDeveloperFactory;
@@ -13,6 +16,9 @@ namespace GoLava.ApplePie
 
         internal Func<IServiceProvider, ILogClient> LogClientFactory;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApplePieOptions"/> class.
+        /// </summary>
         public ApplePieOptions()
         {
             AppleDeveloperFactory = new Func<IServiceProvider, IAppleDeveloperUrlProvider>(_ => new AppleDeveloperUrlProvider());
@@ -20,19 +26,34 @@ namespace GoLava.ApplePie
             LogClientFactory = new Func<IServiceProvider, ILogClient>(_ => new ConsoleLogClient());
         }
 
-        public void UseAppleDeveloper(Func<IServiceProvider, IAppleDeveloperUrlProvider> factory)
+        /// <summary>
+        /// Uses the apple developer URL provider factory provided.
+        /// </summary>
+        /// <param name="factory">The factory to be used.</param>
+        public void UseAppleDeveloperUrlProvider(Func<IServiceProvider, IAppleDeveloperUrlProvider> factory)
         {
-            AppleDeveloperFactory = factory;
+            AppleDeveloperFactory = factory
+                ?? throw new ArgumentNullException(nameof(factory));
         }
 
+        /// <summary>
+        /// Uses the certificate store factory provided.
+        /// </summary>
+        /// <param name="factory">The factory to be used.</param>
         public void UseCertificateStore(Func<IServiceProvider, ICertificateStore> factory)
         {
-            CertificateStoreFactory = factory;
+            CertificateStoreFactory = factory
+                ?? throw new ArgumentNullException(nameof(factory));
         }
 
+        /// <summary>
+        /// Uses the log client factory provided.
+        /// </summary>
+        /// <param name="factory">The factory to be used.</param>
         public void UseLogClient(Func<IServiceProvider, ILogClient> factory)
         {
-            LogClientFactory = factory;
+            LogClientFactory = factory
+                ?? throw new ArgumentNullException(nameof(factory));
         }
     }
 }
